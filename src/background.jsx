167f6c7b58e9f398de542a75b6efd8c46898ffb6
jarodpg <jarod.putman-grain.epitech.eu>
2026-05-24 18:01:45 +0200

    [*] opti background

--- src/background.jsx
+++ src/background.jsx
@@ -1,41 +1,26 @@
 import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import FOG from "vanta/src/vanta.fog";
 
 export default function Background() {
   const vantaRef = useRef(null);
 
   useEffect(() => {
-    let effect;
+    const effect = FOG({
+      el: vantaRef.current, 
+      THREE,
+      mouseControls: true,
+      touchControls: true,
+      highlightColor: 0x000000,
+      midtoneColor: 0xb00909,
+      lowlightColor: 0x1a0000,
+      baseColor: 0x000000,
+      blurFactor: 0.62,
+      speed: 1.4,
+    });
 
-    if (window.VANTA) {
-      effect = window.VANTA.FOG({
-        el: vantaRef.current,
-        mouseControls: true,
-        touchControls: true,
-        highlightColor: 0x000000,
-        midtoneColor: 0xb00909,
-        lowlightColor: 0x1a0000,
-        baseColor: 0x000000,
-        blurFactor: 0.62,
-        speed: 1.4,
-      });
-    }
+    return () => effect?.destroy(); //pr éviter que si le composant ne doit plus s'afficher on l'enlève pour libérer de la mémoire
+  }, []); //crochet de fin permette de le lancer une seul fois 
 
-    return () => {
-      if (effect) effect.destroy();
-    };
-  }, []);
-
-  return (
-    <div
-      ref={vantaRef}
-      style={{
-        position: "fixed",
-        width: "100%",
-        height: "100vh",
-        top: 0,
-        left: 0,
-        zIndex: -1,
-      }}
-    />
-  );
+  return <div ref={vantaRef} style={{ position: "fixed", inset: 0, zIndex: -1 }} />;
 }
